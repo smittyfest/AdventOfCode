9ca91b5c1ffd85414c1d274f1d7bd589cc5408ad
smittyfest <smittyfest@users.noreply.github.com>
2022-12-19 14:21:32 -0600

    Create day01.go
    Solution to Advent of Code 2022 Day 1, part 1.

---
+++ 2022/day01/day01.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println("Error reading file: ", err)
+		return
+	}
+	defer file.Close()
+
+	var max uint64 = 0
+	var accu uint64 = 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if len(line) > 0 {
+			i, err := strconv.ParseUint(line, 10, 64)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			accu += i
+		} else {
+			if accu > max {
+				max = accu
+			}
+			accu = 0
+		}
+	}
+	fmt.Println(max)
+}
